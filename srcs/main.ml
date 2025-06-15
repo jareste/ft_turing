e5ea87bd0b8652199bbeee217a5510feca9b1d58
Joan Areste Gonzalez <jareste2000@gmail.com>
2025-06-15 14:59:13 +0200

    added protection for infinite loops, needing to parse now

--- srcs/main.ml
+++ srcs/main.ml
@@ -1,51 +1,17 @@
-(* 
-let () =
-  if Array.length Sys.argv <> 3 then (
-    Printf.printf "Usage: %s <arg1> <arg2>\n" Sys.argv.(0);
-    exit 1
-  ) else
-    let arg1 = Sys.argv.(1) in
-    let arg2 = Sys.argv.(2) in
-    Printf.printf "Argument 1: %s\nArgument 2: %s\n" arg1 arg2
+module ConfigSet = Set.Make(String)
 
-let add x y = x + y
-let result = add 3 5
-let () = Printf.printf "The result is: %d\n" result
-let () = assert (result = 8)
-let () = Printf.printf "All tests passed!\n" *)
-
-(* starting with real turing *)
+(* TYPEDEFS *)
 type direction = Left | Right
-let direction_of_string = function
-  | "LEFT" -> Left
-  | "RIGHT" -> Right
-  | _ -> failwith "Invalid direction"
-
-let turn dir =
-  match dir with
-  | Left -> Printf.printf "Turning left\n"
-  | Right -> Printf.printf "Turning right\n"
-  (* let () = turn Left
-  let () = turn Right *)
-
 type symbol = char
-let print_symbol s =
-  Printf.printf "Symbol: %c\n" s
-(* let () = print_symbol 'A'
-let () = print_symbol 'B' *)
-
-
 type transition = {
   read: symbol;
   to_state: string;
   write: symbol;
   action: direction;
 }
-
 type state_transitions = (symbol * transition) list
 type key = string * symbol
 type transitions = (key, transition) Hashtbl.t
-
 type turing_machine = {
   name: string;
   alphabet: symbol list;
@@ -55,18 +21,23 @@ type turing_machine = {
   finals: string list;
   transitions: transitions;
 }
-
 type tape = {
   left: symbol list;
   current: symbol;
   right: symbol list;
 }
-
 type config = {
   machine: turing_machine;
   tape: tape;
   state: string;
 }
+(* TYPEDEFS end *)
+
+let direction_of_string = function
+  | "LEFT" -> Left
+  | "RIGHT" -> Right
+  | _ -> failwith "Invalid direction"
+
 
 let create_machine name alphabet blank states initial finals =
   {
@@ -136,7 +107,9 @@ let create_machine_example () =
   let machine = create_machine "unary_sub" alphabet '.' states initial finals in
 
   add_transition machine.transitions "scanright" '.' "scanright" '.' "RIGHT";
-  add_transition machine.transitions "scanright" '1' "scanright" '1' "RIGHT";
+  (* add_transition machine.transitions "scanright" '.' "scanright" '.' "LEFT"; *)
+  (* add_transition machine.transitions "scanright" '1' "scanright" '1' "RIGHT"; *)
+  add_transition machine.transitions "scanright" '1' "scanright" '1' "LEFT";
   add_transition machine.transitions "scanright" '-' "scanright" '-' "RIGHT";
   add_transition machine.transitions "scanright" '=' "eraseone" '.' "LEFT";
 
@@ -151,14 +124,14 @@ let create_machine_example () =
 
   machine
 
-
+(* Turing's machine utils *)
 let update_current tape sym =
   { tape with current = sym }
 
 let move_left tape blank =
   match tape.left with
   | [] ->
-      { left = []; current = blank; right = tape.current :: tape.right }
+      failwith "Cannot move left from the beginning of the tape"
   | hd :: tl ->
       { left    = tl
       ; current = hd
@@ -168,15 +141,12 @@ let move_left tape blank =
 let move_right tape blank =
   match tape.right with
   | [] ->
-      { left    = tape.current :: tape.left
-      ; current = blank
-      ; right   = [] }
+      failwith "Cannot move right from the end of the tape"
   | hd :: tl ->
       { left    = tape.current :: tape.left
       ; current = hd
-      ; right   = tl }
-
-
+      ; right   = tl
+      }
 
 let step config =
   let { state; tape = { current; _ }; machine } = config in
@@ -184,17 +154,16 @@ let step config =
   | None ->
       failwith (Printf.sprintf "No transition for (%s, %c)" state current)
   | Some { to_state; write; action; _ } ->
-      (* 1) write *)
       let tape' = update_current config.tape write in
-      (* 2) move *)
       let tape'' =
         match action with
-        | Left  -> move_left  tape' machine.blank
+        | Left  -> move_left tape' machine.blank
         | Right -> move_right tape' machine.blank
       in
       { machine; state = to_state; tape = tape'' }
-
-
+(* Turing's machine utils END *)
+      
+(* Simple turing, it allows infinite loops *)
 let run machine input =
   let tape = { left = []; current = input.[0]; right = List.init (String.length input - 1) (fun i -> input.[i + 1]) } in
   let config = { machine; tape; state = machine.initial } in
@@ -208,7 +177,7 @@ let run machine input =
     Printf.printf "%s  "
       (let key = (cfg.state, cfg.tape.current) in
        match Hashtbl.find_opt cfg.machine.transitions key with
-       | None -> "HALT"
+       | None -> "ERROR. No transition found\n"
        | Some tr ->
            Printf.sprintf "(%s, %c) -> (%s, %c, %s)\n"
              cfg.state cfg.tape.current
@@ -220,11 +189,47 @@ let run machine input =
   in
   loop config
 
+
+let serialize_config cfg =
+  let tape_str =
+    String.concat ""
+      [ String.of_seq (List.to_seq (List.rev cfg.tape.left));
+        "<"; String.make 1 cfg.tape.current; ">";
+        String.of_seq (List.to_seq cfg.tape.right) ]
+  in
+  cfg.state ^ "|" ^ tape_str
+
+  (* Serializes configuration for avoiding potential infinite loops *)
+let run_detect_cycles machine input =
+  let tape = { left = []; current = input.[0]; right = List.init (String.length input - 1) (fun i -> input.[i + 1]) } in
+  let rec loop cfg seen =
+    let key = serialize_config cfg in
+    if List.mem cfg.state machine.finals then begin
+      Printf.printf "\n";
+      ()
+    end else if ConfigSet.mem key seen then begin
+      failwith (Printf.sprintf "Potential infinite loop detected in configuration:\n%s\n" key);
+    end else begin
+      print_tape cfg.tape cfg.state;
+      Printf.printf "%s  "
+      (let key = (cfg.state, cfg.tape.current) in
+       match Hashtbl.find_opt cfg.machine.transitions key with
+       | None -> "ERROR. No transition found\n"
+       | Some tr ->
+           Printf.sprintf "(%s, %c) -> (%s, %c, %s)\n"
+             cfg.state cfg.tape.current
+             tr.to_state tr.write
+             (match tr.action with Left -> "LEFT" | Right -> "RIGHT")
+      );
+      let seen' = ConfigSet.add key seen in
+      loop (step cfg) seen'
+    end
+  in
+  loop { machine; tape; state = machine.initial } ConfigSet.empty
+
 let () =
 let machine = create_machine_example () in
-let input = "111-11=........." in
+let input = ".111-11=........." in
 print_machine machine;
 
-
-
-run machine input;
+run_detect_cycles machine input;
